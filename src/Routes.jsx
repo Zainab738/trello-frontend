cd8b01d0e5e71e20b0a38119c032f38f338cca9c
Zainab <zainababbas07380@gmail.com>
2025-10-28 10:43:21 +0500

    deleted extra pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -13,7 +13,6 @@ import {
 import ProtectedRoute from "./ProtectedRoutes/ProtectedRoute";
 import ProtectedRoutelogin from "./ProtectedRoutes/ProtectedRoutelogin";
 import ResetPassword from "./pages/ResetPassword";
-import VerifyPassword from "./pages/VerifyPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import PasswordResetSuccess from "./pages/PasswordResetSuccess";
 import Navbar from "../src/components/Navbar";
@@ -27,7 +26,6 @@ function FileRoutesInner() {
       {location.pathname !== "/Login" &&
         location.pathname !== "/Signup" &&
         location.pathname !== "/ResetPassword" &&
-        location.pathname !== "/VerifyPassword" &&
         location.pathname !== "/PasswordResetSuccess" &&
         location.pathname !== "/UpdatePassword" &&
         location.pathname !== "/verification-success" && <Navbar />}
@@ -37,8 +35,7 @@ function FileRoutesInner() {
           <Route path="/" element={<Project />} />
           <Route path="/Tasks/:projectId" element={<Tasks />} />
           <Route path="/createnewproject" element={<CreateNewProject />} />
-
-          {/* /<Route path="*" element={<Navigate to="/" replace />} /> */}
+          /<Route path="*" element={<Navigate to="/" replace />} />
         </Route>
         <Route element={<ProtectedRoutelogin />}>
           <Route path="/Signup" element={<Signup />} />
@@ -48,7 +45,6 @@ function FileRoutesInner() {
           />
           <Route path="/Login" element={<Login />} />
           <Route path="/ResetPassword" element={<ResetPassword />} />
-          <Route path="/VerifyPassword" element={<VerifyPassword />} />
           <Route path="/UpdatePassword" element={<UpdatePassword />} />
           <Route
             path="/PasswordResetSuccess"
